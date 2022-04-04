87922da0246f83ebb10adac1305bf73e8b2d49ca
gids-dadi <gideons564@gmail.com>
2022-04-04 15:50:15 +0100

    Make the mobile nav items disppear

--- script/script.js
+++ script/script.js
@@ -1,7 +1,15 @@
 const menu = document.querySelector('.menu');
 const navMiddle = document.querySelector('.nav-middle');
+
 function toggleMenu() {
   navMiddle.classList.toggle('show');
   menu.classList.toggle('show');
 }
 menu.addEventListener('click', toggleMenu);
+
+document.querySelectorAll('.navMiddle ul li').forEach((n) =>
+  n.addEventListener('click', () => {
+    menu.classList.remove('show');
+    navMiddle.classList.remove('show');
+  })
+);
