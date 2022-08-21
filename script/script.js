e575a99aead0217023cb70bb39cdf5573c96702b
yayner <yayner2002@gmail.com>
2022-08-21 11:04:14 +0300

    Adjust the my recent works section

--- script/script.js
+++ script/script.js
@@ -36,7 +36,7 @@ const projectDetail = [{
   seeSource: 'https://github.com/yayner2002/Leaderboard',
 },
 {
-  title: 'Admas University Webiste',
+  title: 'University Webiste',
   popTitle: 'Keeping track of hundreds of components',
   popDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the relea',
   technologies: ['Bootstrap', 'CSS', 'JavaScript', 'HTML'],
